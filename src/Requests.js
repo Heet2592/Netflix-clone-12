83037a6cb9d7e01185418ac3f60ab43bfb187209
Heet2592 <55369646+Heet2592@users.noreply.github.com>
2021-06-15 12:51:43 -0400

    Update Requests.js

--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,4 @@
- const API_KEY = "91106a9116ec466799a512dab41518ce";
+ //const API_KEY = "91106a9116ec466799a512dab41518ce";
 
  const requests = {
     fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-US`,
